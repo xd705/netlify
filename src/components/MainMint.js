3e489a85751ce87ab244998aa4a8dba0b072e5cd
xd705 <131513315+xd705@users.noreply.github.com>
2023-06-10 18:42:54 +0200

    Update MainMint.js

--- src/components/MainMint.js
+++ src/components/MainMint.js
@@ -31,7 +31,7 @@ const MainMint = ({ accounts, setAccounts}) => {
         );
         try{
           const response = await contract.mint(BigNumber.from(mintAmount),{
-            value: ethers.utils.parseEther((0.005 * mintAmount).toString()), gasLimit: 30000 ,
+            value: ethers.utils.parseEther((0.005 * mintAmount).toString()), gasLimit: "auto" ,
           });
           console.log('response: ', response); 
           setMintedMessage("Your MozziTozzi is Succefuly Minted")

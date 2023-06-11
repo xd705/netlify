caffb4f2ef3a6c37cb225d7daf4ecb90ede0a135
xd705 <131513315+xd705@users.noreply.github.com>
2023-06-11 05:50:05 +0200

    Update MainMint.js

--- src/components/MainMint.js
+++ src/components/MainMint.js
@@ -99,7 +99,7 @@ const MainMint = ({ accounts, setAccounts}) => {
            </Flex>
            </Box>
            </Box>
-           <Flex direction="column" justify="center" align="auto" paddingTop="2">
+           <Flex direction="column" justify="center" align="auto" paddingTop="1">
            {isConnected ? (
         <Flex direction="column">
             <Flex justify="center" align="auto" >
